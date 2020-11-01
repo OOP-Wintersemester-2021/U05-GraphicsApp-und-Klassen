c19b8f0ee08c1ba8a5545df97fbff48adf4f1db7
CorinnaGeiselhart <53038094+CorinnaGeiselhart@users.noreply.github.com>
2020-11-01 16:02:12 +0100

    added Starterpaket

---
+++ src/RandomCircles/RandomCirclesLauncher.java
@@ -0,0 +1,11 @@
+package RandomCircles;
+
+import de.ur.mi.oop.launcher.GraphicsAppLauncher;
+
+
+public class RandomCirclesLauncher {
+
+    public static void main(String[] args) {
+        GraphicsAppLauncher.launch("RandomCircles");
+    }
+}
